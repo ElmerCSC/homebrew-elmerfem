20f27c38912d2b8ba91ab6418bac221940ed6d1f
Juhani Kataja <juhani.kataja@csc.fi>
2017-01-20 09:47:22 +0200

    Added option for gcc heuristics

--- elmer.rb
+++ elmer.rb
@@ -14,6 +14,7 @@ class Elmer < Formula
   option "with-openmp", "Enable OpenMP support (experimental)"
   option "with-testing", "Run the quick tests"
   option "with-qt5", "Use qt5 with ElmerGUI"
+  option "with-find-gcc", "Use heuristics to find the gcc compiler binary"
 
   depends_on :mpi => [:f90, :recommended]
 
@@ -25,10 +26,10 @@ class Elmer < Formula
   depends_on "hypre" => :recommended
   depends_on "mumps" => :recommended
 
-  depends_on "qt5" [if build.with? "elmergui" and build.with? "qt5", :recommended]
+  depends_on "qt5" => :recommended if build.with? "elmergui" and build.with? "qt5" 
   depends_on "qt" if build.with? "elmergui" and not build.with? "qt5"
   depends_on "oce" if build.with? "elmergui"
-  depends_on "vtk" => ["with-qt" if build.with? "elmergui", :optional]
+  depends_on "vtk" => :optional if build.with? "elmergui"
   depends_on "qwt" if build.with? "elmergui"
 
   def install
@@ -45,10 +46,9 @@ class Elmer < Formula
     cmake_args << "-DBLAS_LIBRARIES:STRING=#{Formula["openblas"].opt_lib}/libopenblas.#{exten};-lpthread"
     cmake_args << "-DLAPACK_LIBRARIES:STRING=#{Formula["openblas"].opt_lib}/libopenblas.#{exten};-lpthread"
 
-    ENV["CC"] = Dir["#{Formula["gcc"].opt_bin}/gcc*"].select{ |f| /gcc-\d+.*$/.match(f) }.first
-    ENV["CXX"] = Dir["#{Formula["gcc"].opt_bin}/g++*"].select{ |f| /g[+][+]-\d+.*$/.match(f) }.first
-    # ENV["CC"] = "#{Formula["gcc"].opt_bin}/gcc-#{Formula["gcc"].version_suffix}"
-    # ENV["CXX"] = "#{Formula["gcc"].opt_bin}/g++-#{Formula["gcc"].version_suffix}"
+    ENV["CC"] = Dir["#{Formula["gcc"].opt_bin}/gcc*"].select{ |f| /gcc-\d+.*$/.match(f) }.first if build.with? "find-gcc"
+    ENV["CXX"] = Dir["#{Formula["gcc"].opt_bin}/g++*"].select{ |f| /g[+][+]-\d+.*$/.match(f) }.first if build.with? "find-gcc"
+
     if build.with? "elmergui"
       cmake_args << "-DWITH_ELMERGUI:BOOL=TRUE"
       cmake_args << "-DWITH_QWT:BOOL=TRUE"
